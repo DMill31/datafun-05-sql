b75d2d6faf7444b3145d545b6ad7f56f4eb1d96e
Dan <danmiller0831@gmail.com>
2025-02-10 12:18:08 -0600

    Added INSERT and the python code to implement it

--- db01_setup.py
+++ db01_setup.py
@@ -15,6 +15,7 @@ def create_db():
     except sqlite3.Error as e:
         logger.error("Error creating database:", e)
 
+
 def drop_tables():
     """Function that reads and executes SQL statements to drop tables."""
     try:
@@ -42,9 +43,29 @@ def create_tables():
     except sqlite3.Error as e:
         logger.error("Error creating tables:", e)
 
+
+def insert_data_from_csv():
+    """Function to use pandas to read data from CSV files (in 'data' folder)
+    and insert the records into their respective tables."""
+    try:
+        author_data_path = pathlib.Path("data", "authors.csv")
+        book_data_path = pathlib.Path("data", "books.csv")
+        authors_df = pd.read_csv(author_data_path)
+        books_df = pd.read_csv(book_data_path)
+        with sqlite3.connect(db_file) as conn:
+            # Use the pandas DataFrame to_sql() method to insert data
+            # pass in the table name and the connection
+            authors_df.to_sql("authors", conn, if_exists="replace", index=False)
+            books_df.to_sql("books", conn, if_exists="replace", index=False)
+            print("Data inserted successfully.")
+    except (sqlite3.Error, pd.errors.EmptyDataError, FileNotFoundError) as e:
+        print("Error inserting data:", e)
+
+
 def main():
     create_db()
     create_tables()
+    insert_data_from_csv()
 
 if __name__ == "__main__":
     main()
