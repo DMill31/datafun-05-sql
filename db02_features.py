243dc100ba71fef5ae5ca889a1aa01258a8a66a9
Dan <danmiller0831@gmail.com>
2025-02-11 08:18:57 -0600

    Added UPDATE and the python code to implement it

---
+++ db02_features.py
@@ -0,0 +1,24 @@
+import sqlite3
+import pathlib
+from utils_logger import logger
+
+db_file = pathlib.Path("project.sqlite3")  # Path to the SQLite database file
+
+def update_db():
+    """Update the database with new features."""
+    try:
+        with sqlite3.connect(db_file) as conn:
+            sql_file = pathlib.Path("sql_features", "update_records.sql")
+            with open(sql_file, "r") as file:
+                sql_script = file.read()
+            conn.executescript(sql_script)
+            logger.info("Database updated")
+    except sqlite3.Error as e:
+        logger.error("Error updating database:", e)
+
+
+def main():
+    update_db()
+
+if __name__ == "__main__":
+    main()
---
+++ sql_features/update_records.sql
@@ -0,0 +1,5 @@
+-- Update a record in the specified table
+
+UPDATE books
+SET title = "Harry Potter and the Sorcerer's Stone"
+WHERE book_id = 'ca8e64c3-1e67-47f5-82cc-3e4e30f63b75';
